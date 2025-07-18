fd0abbe42fc6db2fde31a4cfd5ee9b28879ecd5a
Sparisoma Viridi <dudung@gmail.com>
2025-07-18 21:26:38 +0700

    update

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+# flask --app app run --debug --port=5500
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "🚀 Hello from Flask on Render! dd"
+def home():
+    return "Hello from Python run!"
+
+@app.route("/about")
+def about():
+    return "This is the About page."
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route("/search")
+def search():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    city = request.args.get("city")
+    return f"Name: {name}, Age: {age}, City: {city}"
 
 if __name__ == "__main__":
     app.run(debug=True)

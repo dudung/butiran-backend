972abd63141b780879b4515d551ddc8669d2af28
Sparisoma Viridi <dudung@gmail.com>
2025-07-18 23:34:22 +0700

    update

--- app.py
+++ app.py
@@ -6,22 +6,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python run!"
-
-@app.route("/about")
-def about():
-    return "This is the About page."
-
-@app.route("/hello/<name>")
-def hello(name):
-    return f"Hello, {name}!"
-
-@app.route("/search")
-def search():
-    name = request.args.get("name")
-    age = request.args.get("age")
-    city = request.args.get("city")
-    return f"Name: {name}, Age: {age}, City: {city}"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return "a response from butiran-backend"

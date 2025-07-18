303ee21c4cccb0f1f13795085e1f0743618ef329
Sparisoma Viridi <dudung@gmail.com>
2025-07-18 21:02:08 +0700

    udate

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Hello from Flask on Render!"
+    return "🚀 Hello from Flask on Render! dd"
+
+if __name__ == "__main__":
+    app.run(debug=True)

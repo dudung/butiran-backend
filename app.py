bb347d417fc985f6af02a44ec1250b9e681e0e1f
Sparisoma Viridi <dudung@gmail.com>
2025-07-19 09:25:33 +0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # flask --app app run --debug --port=5500
 
-from flask import Flask, request
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes
 
 @app.route("/")
 def home():
